253ab8f17244c2903cf4a81d8bebec4710ab4732
olayinka sanyaolu <yellowbounty55@gmail.com>
2025-07-10 13:53:28 +0100

    initial commit with react app

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -2,6 +2,10 @@ import './App.css';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import WelcomeMessage from './components/WelcomeMessage';
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 
 function App() {
   return (
