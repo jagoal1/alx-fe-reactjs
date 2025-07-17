c4016ff8cf3bb379fbd8a518e168128678a6098e
olayinka sanyaolu <yellowbounty55@gmail.com>
2025-07-17 16:28:52 +0100

    Added inline CSS styling to components in alx-react-app-new

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f2f2f2' }}>
+      <p style={{ fontSize: '18px' }}>Explore the cities you love the most!</p>
+    </main>
+  );
+}
+
+export default MainContent;
