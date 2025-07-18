3724243d46feb079adcf02db37854bc26a216d43
olayinka sanyaolu <yellowbounty55@gmail.com>
2025-07-18 10:36:32 +0100

    modify userprofile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,13 @@
-function UserProfile(props) {
+import { useContext } from 'react';
+import UserContext from '../UserContext';
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-      <h2 style={{ color: 'blue', fontSize: '24px' }}>{props.name}</h2>
-      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-      <p>Bio: {props.bio}</p>
+    <div>
+      <h2>Name: {userData.name}</h2>
+      <p>Email: {userData.email}</p>
     </div>
   );
 }
