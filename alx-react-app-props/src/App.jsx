77356f2c65139ed3d0345d87471008ef8f943834
olayinka sanyaolu <yellowbounty55@gmail.com>
2025-07-18 10:28:22 +0100

    added user date to app.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,7 +2,7 @@ import UserContext from './UserContext';
 import ProfilePage from './ProfilePage';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Olayinka Sanyaolu", email: "yeankahsanya@yahoo.com" };
 
   return (
     <UserContext.Provider value={userData}>
