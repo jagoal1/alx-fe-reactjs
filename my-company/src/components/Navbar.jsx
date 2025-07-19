b13c9d64f90d62c873635463493f58079f4209de
olayinka sanyaolu <yellowbounty55@gmail.com>
2025-07-19 09:04:38 +0100

    updated navbar.jsx to display justifycontent

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,12 +1,29 @@
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: '#333', padding: '10px' }}>
-      <Link to="/" style={{ margin: '0 10px', color: 'white' }}>Home</Link>
-      <Link to="/about" style={{ margin: '0 10px', color: 'white' }}>About</Link>
-      <Link to="/services" style={{ margin: '0 10px', color: 'white' }}>Services</Link>
-      <Link to="/contact" style={{ margin: '0 10px', color: 'white' }}>Contact</Link>
+    <nav
+      style={{
+        backgroundColor: "navy",
+        padding: "10px",
+        display: "flex",
+        justifyContent: "center",
+        gap: "20px",
+      }}
+    >
+      <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+        Home
+      </Link>
+      <Link to="/about" style={{ color: "white", textDecoration: "none" }}>
+        About
+      </Link>
+      <Link to="/services" style={{ color: "white", textDecoration: "none" }}>
+        Services
+      </Link>
+      <Link to="/contact" style={{ color: "white", textDecoration: "none" }}>
+        Contact
+      </Link>
     </nav>
   );
 }
